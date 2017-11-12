91f5001b47b939e3eebfe6be8beb7b581c1b5b4c
James Yuzawa <jyuzawa@triplelift.com>
2017-11-12 11:47:07 -0500

    use a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// loads various gulp modules
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-clean-css');
+var webpack = require('webpack');
+var gulpWebpack = require('webpack-stream');
+
+// create task
+gulp.task("default", ["css","js"]);
+
+gulp.task('css', function(){
+    gulp.src(['src/css/**/*.css','node_modules/leaflet/dist/*.css'])
+        .pipe(minifyCSS())
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('dist'))
+    gulp.src('node_modules/leaflet/dist/images/*')
+        .pipe(gulp.dest('dist/images'))
+    gulp.src('src/img/*')
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('js', function(){
+    // modify some webpack config options
+    var webpackConfig = require('./webpack.config.js')
+    webpackConfig.plugins = [
+    new webpack.DefinePlugin({
+        "process.env": {
+            // This has effect on the react lib size
+            "NODE_ENV": JSON.stringify("production")
+        }
+    }), new webpack.optimize.UglifyJsPlugin()];
+
+    // run webpack
+    return gulp.src('src/js/main.js')
+      .pipe(gulpWebpack( webpackConfig ))
+      .pipe(gulp.dest('dist'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var CopyTask = require('copy-webpack-plugin')
 module.exports = {
     entry: './src/js/main.js',
     output: {
@@ -11,24 +10,5 @@ module.exports = {
             // this module has dot in its name which is screwing shit up
             "compass-js": path.resolve(__dirname, "node_modules/compass.js/lib/compass.js")
         }
-    },
-    plugins: [
-    new CopyTask([
-        {
-            from: "src/css/*",
-            to: "css",
-            flatten: true
-        },
-        {
-            from: "src/img/*",
-            to: "img",
-            flatten: true
-        },{
-        from: "node_modules/leaflet/dist/leaflet.css",
-        to: "leaflet"
-    }, {
-        from: "node_modules/leaflet/dist/images/*",
-        to: "leaflet/images",
-        flatten: true
-    }]), ]
+    }
 };
