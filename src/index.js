99f4f6d3677162de5781d59a14d0326da6326ab6
James Yuzawa <jtyuzawa@gmail.com>
2019-02-10 12:04:28 -0500

    rename

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { hcl } from 'd3-color';
 import {DEFAULT_HUE, MAIN_LUMINANCE, ALT_LUMINANCE} from './components/HueControl';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import App from './components/App';
-import LocationService from './components/LocationService';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 const hue = parseInt(localStorage.getItem("color") || DEFAULT_HUE, 10);
@@ -25,7 +24,7 @@ const theme = createMuiTheme({
 const ThemedApp = () => {
 	return (
 		<MuiThemeProvider theme={theme}>
-			<LocationService />
+			<App/>
 		</MuiThemeProvider>
 	);
 }
